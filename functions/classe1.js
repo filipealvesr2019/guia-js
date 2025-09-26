fbe15137856870734c2192970dfc49037cc00846
opaulorom <filipealvesrds2019@gmail.com>
2025-09-26 14:17:24 -0300

    add

--- functions/classe1.js
+++ functions/classe1.js
@@ -5,6 +5,14 @@ class Pessoa{
     }
 
     apresentar() {
-        console.log(`Oi, eu sou`)
+        console.log(`Oi, eu sou ${this.nome} e tenho ${this.idade} anos.`)
     }
 }
+
+const pessoa1 = new Pessoa("Ana", 25);
+const pessoa2 = new Pessoa("João", 30);
+const pessoa3 = new Pessoa("Maria", 40);
+
+pessoa1.apresentar();
+pessoa2.apresentar();
+pessoa3.apresentar();
