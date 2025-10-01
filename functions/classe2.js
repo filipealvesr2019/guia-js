f0158259efce1c5ee7aa41a0a4e332f49d81fb41
opaulorom <filipealvesrds2019@gmail.com>
2025-10-01 14:59:42 -0300

    add

--- functions/classe2.js
+++ functions/classe2.js
@@ -1,4 +1,4 @@
-class contaBancaria {
+class ContaBancaria {
     constructor(titular, saldo = 0) {
         this.titular = titular;
         this.saldo = saldo;
@@ -8,4 +8,17 @@ class contaBancaria {
         this.saldo += valor;
         console.log(`Deposito de R$${valor}. Saldo atual: R$${this.saldo}`)
     }
+
+    sacar(valor){
+        if(valor > this.saldo){
+            console.log('Saldo insuficiente!')
+        } else {
+            this.saldo -= valor;
+            console.log(`Saque de R$${valor}. Saldo atual R$${this.saldo}`)
+        }
+    }
 }
+
+const conta = new ContaBancaria("Carlos", 100);
+conta.depositar(50);
+conta.sacar(200);
