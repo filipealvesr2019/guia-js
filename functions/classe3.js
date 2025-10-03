c2fdfc202770551927b88ab9b323debdf5692133
opaulorom <filipealvesrds2019@gmail.com>
2025-10-03 15:25:08 -0300

    add

--- functions/classe3.js
+++ functions/classe3.js
@@ -3,4 +3,12 @@ class Carro {
         this.marca= marca;
         this.ano = ano;
     }
+
+    detalhes(){
+        console.log(`Carro: ${this.marca}, Ano: ${this.ano}`)
+    }
 }
+
+// criando um objeto a partir da classe
+
+const carro = new Carro('Ford', 2004);
