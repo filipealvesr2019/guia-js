32c1bfc27fe57af3cfc532089ee6eddcbb437c3b
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-10-09 14:42:50 -0300

    add

--- functions/classe4.js
+++ functions/classe4.js
@@ -6,5 +6,14 @@ class Veiculo {
 }
 
 class Moto extends Veiculo {
-    
+    constructor(marca, ano){
+        super(marca, ano)
+    }
+
+    detalhes(){
+        console.log(`Moto ${this.marca}, Ano ${this.ano}`)
+    }
 }
+
+const moto = new Moto('Yamaha', 2021);
+moto.detalhes();
