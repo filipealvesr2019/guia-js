479141bbd8d50a6bd99d27a4b0e532a30c3aea37
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-10-13 15:14:43 -0300

    add

--- functions/exercicio5.js
+++ functions/exercicio5.js
@@ -1,3 +1,17 @@
 class Pessoa {
-    
+    constructor(nome){
+        this._nome = nome;
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    set nome(novoNome) {
+        if (novoNome.length < 3){
+            console.log("Nome muito curto!");
+        } else{
+            this._nome = novoNome;
+        }
+    }
 }
