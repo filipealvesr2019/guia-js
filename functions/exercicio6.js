ee87d6c55aba123c6df7dbe72e2b35c116e93aa8
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-10-15 14:46:28 -0300

    add

--- functions/exercicio6.js
+++ functions/exercicio6.js
@@ -1,5 +1,14 @@
 class Retangulo {
     constructor(largura, altura){
+        this.largura = largura;
+        this.altura = altura;
         
     }
+
+    get area() {
+        return this.largura * this.altura;
+    }
 }
+
+const retangulo = new Retangulo(5, 4);
+console.log(retangulo.area)
