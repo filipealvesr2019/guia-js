0f81fc3d3121211cd73c8f221b3b866d30922176
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-10-17 15:07:08 -0300

    add

--- functions/classe5.js
+++ functions/classe5.js
@@ -1,6 +1,18 @@
 class Produto{
     constructor(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+    }
 
+    set precoPositivo(precoAtual){
+        if (precoAtual.length < 3){
+
+        }
+
+    }
+
+    get precoPositivo(){
+        
     }
     
 }
