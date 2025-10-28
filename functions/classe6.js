206d9bb03700ce5d19228480f2cc1067590d6f71
opaulorom <paulinhop2017@gmail.com>
2025-10-28 15:10:48 -0300

    add

--- functions/classe6.js
+++ functions/classe6.js
@@ -1,3 +1,7 @@
 const pontosJogadores = new Map();
 
 pontosJogadores.set("jodador1", 10);
+pontosJogadores.set("jogador2", 20);
+pontosJogadores.set("jogador3", 30);
+
+console.log(pontosJogadores.get("jogador1"));
