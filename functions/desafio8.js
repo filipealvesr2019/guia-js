35c3c81bae38cc306903b49c61b08680f8780d58
opaulorom <paulinhop2017@gmail.com>
2025-10-31 14:51:22 -0300

    add

--- functions/desafio8.js
+++ functions/desafio8.js
@@ -1,3 +1,9 @@
 let nome = prompt("Digite seu nome: ");
 let fase1 = Number(prompt("Pontuação na fase 1: "));
-let
+let fase2 = Number(prompt("Pontuação na fase 2: "));
+let fase3 = Number(prompt("Pontuação na fase 3: "));
+
+let media = (fase1 + fase2 + fase3) / 3;
+
+let resultado = media >= 70 ? `Parabens, ${nome}! Voce venceu! 🎉` : `Tente Novamenete, ${nome}. 😞`;
+
