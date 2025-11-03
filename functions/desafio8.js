b8e47739da05ca8e520cf329c89d2d57b92511ce
opaulorom <paulinhop2017@gmail.com>
2025-11-03 14:52:50 -0300

    add

--- functions/desafio8.js
+++ functions/desafio8.js
@@ -1,9 +1,14 @@
-let nome = prompt("Digite seu nome: ");
-let fase1 = Number(prompt("Pontuação na fase 1: "));
-let fase2 = Number(prompt("Pontuação na fase 2: "));
-let fase3 = Number(prompt("Pontuação na fase 3: "));
+async function main() {
+    let nome = prompt("Digite seu nome: ");
+    let fase1 = Number(prompt("Pontuação na fase 1: "));
+    let fase2 = Number(prompt("Pontuação na fase 2: "));
+    let fase3 = Number(prompt("Pontuação na fase 3: "));
+    
+    let media = (fase1 + fase2 + fase3) / 3;
+    
+    let resultado = media >= 70 ? `Parabens, ${nome}! Voce venceu! 🎉` : `Tente Novamenete, ${nome}. 😞`;
+    
+    console.log(`Sua media foi: ${media}`);
+    console.log(resultado);
 
-let media = (fase1 + fase2 + fase3) / 3;
-
-let resultado = media >= 70 ? `Parabens, ${nome}! Voce venceu! 🎉` : `Tente Novamenete, ${nome}. 😞`;
-
+}
