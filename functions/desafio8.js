a353da8abb810ff5ddf4a00949208383f8226cff
opaulorom <paulinhop2017@gmail.com>
2025-11-04 14:32:00 -0300

    add

--- functions/desafio8.js
+++ functions/desafio8.js
@@ -1,8 +1,15 @@
+
+function perguntar(pergunta) {
+    return new Promise(resolve => {
+        rl.question(pergunta, resposta)
+    })
+}
+
 async function main() {
-    let nome = prompt("Digite seu nome: ");
-    let fase1 = Number(prompt("Pontuação na fase 1: "));
-    let fase2 = Number(prompt("Pontuação na fase 2: "));
-    let fase3 = Number(prompt("Pontuação na fase 3: "));
+    let nome = await perguntar("Digite seu nome: ");
+    let fase1 = Number(await perguntar("Pontuação na fase 1: "));
+    let fase2 = Number(await perguntar("Pontuação na fase 2: "));
+    let fase3 = Number(await perguntar("Pontuação na fase 3: "));
     
     let media = (fase1 + fase2 + fase3) / 3;
     
